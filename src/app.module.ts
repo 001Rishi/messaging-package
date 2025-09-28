f193891bad637be562970dd6c23fdf0258a72c5f
001Rishi <rishikesh.singh@techintelleon.com>
2025-09-28 18:54:48 +0530

    fix: export issue

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { RabbitMQCommunicationModule } from './modules/rabbitmq-communications.module';
 
 @Module({
-  imports: [],
+  imports: [RabbitMQCommunicationModule],
   controllers: [],
   providers: [],
+  exports: [RabbitMQCommunicationModule],
 })
 export class AppModule {}
