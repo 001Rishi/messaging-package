934278fa21237e8b2cecb3cd90347635249adb4d
Rishikesh <rishi@Rishikeshs-MacBook-Pro.local>
2025-09-28 21:02:55 +0530

    fix: added export changes

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export * from './modules/rabbitmq-communications.module';
+export * from './decorators/rabbitmq-handler.decorator';
+export * from './interfaces/rabbitmq-config.interface';
+export * from './services/rabbitmq-client.service';
+export * from './services/message-handler.service';
